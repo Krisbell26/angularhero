ce2639e1a079f946a68c38a0bef2704b2f2740c6
Krisbell26 <krisbelldrnw@gmail.com>
2019-05-10 10:10:18 +0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/weblanjut'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+  res.status(200).sendFile(__dirname + '/weblanjut/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
